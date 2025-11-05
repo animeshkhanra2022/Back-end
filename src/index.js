9d628141d7bd48d091532e144c759e0f3d11b2e1
Animesh Khanra <animeshkhanra2022@gmail.com>
2025-11-05 11:30:13 +0530

    feat: add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/index.js" 
 import dotenv from "dotenv"
+import {app} from "./app.js"
 
 
 dotenv.config({
@@ -7,3 +8,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+
+    app.on("Error", (error) => {
+        console.log("Error::", error);
+        throw error;
+    })
+
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port http://localhost:${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed:: error", error)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//     try {
+//         await fun(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 5000).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
